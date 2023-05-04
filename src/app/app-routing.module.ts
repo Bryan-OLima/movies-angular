0117678e972853b5e9c78c088c8155a77b169004
BryanOLima <bryanosoriolima@gmail.com>
2023-05-04 18:32:53 -0300

    Nav bar done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const title: string = 'The Movies'
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    title: `${title} - Home`
+  },
+  {
+    path: 'sobre',
+    component: AboutPageComponent,
+    title: `${title} - Sobre`
+  },
+  {
+    path: '',
+    component: NotFoundPageComponent,
+    title: `${title} - 404`
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
