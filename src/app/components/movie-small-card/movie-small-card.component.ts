48bd9ac6951f1083d4d2cfc7e7ef9ad16fefb9cc
BryanOLima <bryanosoriolima@gmail.com>
2023-05-06 15:54:51 -0300

    [bug] fixed - like button not appearing

--- src/app/components/movie-small-card/movie-small-card.component.ts
+++ src/app/components/movie-small-card/movie-small-card.component.ts
@@ -8,15 +8,19 @@ import { Component, Input, OnChanges } from '@angular/core';
 export class MovieSmallCardComponent implements OnChanges {
 
   @Input() public poster: any = '';
+  @Input() public rating: any = '';
+  @Input() public type: any = '';
   @Input() public name: any = '';
   @Input() public id: any = '';
   
   public posterLink: string = `https://image.tmdb.org/t/p/w500`;
 
   ngOnChanges(){
-    console.log(this.poster, this.name, this.id);
-
+    console.log(this.poster, this.name, this.id, this.rating, this.type);
+    this.rating = Number(this.rating);
   }
+
+
 
   testFunction(texto: string){
     console.log(texto);
